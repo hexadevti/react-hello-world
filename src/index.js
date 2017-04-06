71f31dd36737e83c762294fdb7d49245e448bd95
Luciano Faria <luciano.faria@gentrop.com>
2017-04-06 20:31:38 -0300

    vai

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App2 from './App';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App2 />,
-  document.getElementById('root')
+  <App />,
+  document.getElementById('root') 
 );
